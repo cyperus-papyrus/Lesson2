4fc963a051c5a0cf466a27b5dd04ded30ff80721
Helga Rais <cdg013@gmail.com>
2018-09-11 02:23:15 +0300

    Добавлены задания для цикла whyle и try\except

---
+++ cycle_while/smalltalk.py
@@ -0,0 +1,29 @@
+# Напишите функцию ask_user(), которая с помощью input() спрашивает пользователя “Как дела?”,
+# пока он не ответит “Хорошо”. Создайте словарь типа "вопрос": "ответ", например:
+# {"Как дела": "Хорошо!", "Что делаешь?": "Программирую"} и так далее.
+# Доработайте ask_user() так, чтобы когда пользователь вводил вопрос который есть в словаре, программа давала ему
+# соотвествующий ответ. Например:
+# Пользователь: Что делаешь?
+# Программа: Программирую
+
+
+def ask_user(questions_answers):
+    user_say = str(input('Как дела? \n>>'))
+    while True:
+        try:
+            if user_say in questions_answers.keys():
+                user_say = str(input(questions_answers[user_say] + '\n>>'))
+                continue
+            elif user_say == 'Хорошо':
+                break
+            else:
+                user_say = str(input('Как дела? \n>>'))
+                continue
+        except KeyboardInterrupt:
+            print('\nПока!')
+            break
+
+
+answers_dict = {"Привет": "Привет!", "Как дела?": "Хорошо!", "Что делаешь?": "Программирую",
+                "На каком языке?": "Естественно, на Python!"}
+ask_user(answers_dict)
---
+++ exceptions/sum.py
@@ -0,0 +1,16 @@
+# Напишите функцию get_summ(num_one, num_two), которая принимает на вход два целых числа (int) и складывает их
+# Оба аргумента нужно приводить к целому числу при помощи int() и перехватывать исключение ValueError если приведение
+# типов не сработало
+
+
+def get_sum(num_one, num_two):
+    try:
+        sum = int(num_one) + int(num_two)
+        print(sum)
+    except ValueError:
+        print('Не все переменные - цифры!1')
+
+
+get_sum(1234.9932, '3332')
+get_sum('sdlksdlksdl', '3332')
+get_sum(123+3.44, 12/3.99)
